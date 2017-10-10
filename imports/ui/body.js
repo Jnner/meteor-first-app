8fabeb11d1d87d54e9ca63a347fe317dd0c93dc8
jnner <j@nner.es>
2017-10-10 21:53:21 +0200

    v1.3 sustituimos el mock tareas, por la colección tareas en MongoDB

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,14 @@
 import {Template} from 'meteor/templating';
 
+import {Tareas} from '../api/tareas.js';
+
 import './body.html';
 
 Template.body.helpers({
-  tareas: [
-     {texto: 'esto es la tarea numero 1'}
-    ,{texto: 'esto es la tarea numero 2'}
-    ,{texto: 'esto es la tarea numero 3'}
-  ]
+  tareas () {
+    return Tareas.find({});
+  }
+  // Para que surja la magia,insertamos por consola los datos a nuestra BD de Mongo:
+  // $ meteor mongo
+  // $ db.tareas.insert({ texto: "Hola mundo!", fechaCreado: new Date() });
 });
